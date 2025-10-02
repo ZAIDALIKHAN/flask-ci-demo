cc465567e4f3013c0fadc57088ea0b26e04ec3c3
Ubuntu <ubuntu@ip-172-31-22-153.ec2.internal>
2025-10-02 12:28:58 +0000

    Updated homepage message

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask CI/CD Demo!"
-
+    #return "Hello from Flask CI/CD Demo!"
+    return "Hello from Flask CI/CD Demo! v2"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
